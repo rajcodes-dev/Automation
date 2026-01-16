8205d313a4bd25e305cf42128fcc20446abcbe5e
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-16 16:02:24 +0530

    added vowels

--- Regex/main.py
+++ Regex/main.py
@@ -1,4 +1,8 @@
 import re
-pattern = re.compile(r'\d{3}-\d{3}-\d{4}')
-match = pattern.search('my phone number is 323-239-9833.')
-print(match.group())
+# pattern = re.compile(r'\d{3}-\d{3}-\d{4}')
+# match = pattern.search('my phone number is 323-239-9833.')
+# print(match.group())
+
+vowel_pattern = re.compile(r"[^aeiouAEIOU]")
+# vowel_pattern = re.compile(r"[aeiouAEIOU]")
+print(vowel_pattern.findall('consistency matters'))
