5f5d8cb852fe58e0f14df05302a5559120eff58d
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-18 15:14:00 +0530

    added argument

--- Regex/regex_strip.py
+++ Regex/regex_strip.py
@@ -1,7 +1,11 @@
 """Regex version of strip() function."""
 import re
 
-def regex_strip(text):
-    return re.sub(r'^\s+|\s+$','',text)
+def regex_strip(text, chars = None):
+    if chars is None:
+        return re.sub(r'^\s+|\s+$','',text)
+    else:
+        pattern = '^[' + chars + ']+' + '|[' + chars + ']+$'
+        return re.sub(pattern, '', text)
 
-print(regex_strip('  sdf  jk    '))
+print(regex_strip('*****sdfjk****', '*'))
