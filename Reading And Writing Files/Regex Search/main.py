5bbf61cb2bfc558f10c883178c476bebcbfacb28
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-22 08:36:55 +0530

    refined version

--- Reading And Writing Files/Regex Search/main.py
+++ Reading And Writing Files/Regex Search/main.py
@@ -1,17 +1,16 @@
 from pathlib import Path
-import os, re
+import re
 
-text = re.compile(r"hi everyone.")
+text = re.compile(r"hi everyone\.")
 
 p = Path(r"D:\PROGRAMMES\Code Arena\Automation\Reading And Writing Files\Regex Search")
-os.chdir(p)
-pattern = re.compile(r"(\.txt)$")
-dirs = os.listdir(p)
-print(dirs)
 
-for dir in dirs:
-    if pattern.search(dir) is not None:
-        with open(dir, 'r', encoding='utf-8') as file:
-            content = file.readlines()
-            if text.search(content) is not None:
-                print(dir)
+if p.exists():
+    for file_path in p.glob('*.txt'):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            for line in file:
+                if text.search(line):
+                    print(f"Match Found: {file_path.name}")
+                    print(f"line content: {line.strip()}")
+else:
+    print("The specified path does not exist!")
