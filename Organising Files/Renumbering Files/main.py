435304bd8eedc7ebd98db0da81b6cbfbd0601c4e
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-24 09:52:14 +0530

    added a loop

--- Organising Files/Renumbering Files/main.py
+++ Organising Files/Renumbering Files/main.py
@@ -1,9 +1,14 @@
 """Program which renumber the files name."""
-import os
+import re
 from pathlib import Path
 
-os.chdir("D:\PROGRAMMES\Code Arena\Automation\Organising Files\Renumbering Files")
-for i in range(1, 121):
-    if i not in (42, 86, 103):
-        with open(f'spam{str(i).zfill(3)}.txt', 'w') as file:
-            pass
+p = Path("D:\PROGRAMMES\Code Arena\Automation\Organising Files\Renumbering Files")
+# for i in range(1, 121):
+#     if i not in (42, 86, 103):
+#         with open(f'spam{str(i).zfill(3)}.txt', 'w') as file:
+#             pass
+pattern = re.compile(r'\w+(\d{3})\.txt')
+for idx, file_name in enumerate(p.glob("spam*.txt")):
+    file_name = file_name.parts[-1]
+    num = pattern.search(file_name)
+    num = num.group(1)
