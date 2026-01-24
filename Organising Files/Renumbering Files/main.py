241553a94f7c09a911d4e4ee082a6b32534144b5
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-24 10:05:58 +0530

    prototype added

--- Organising Files/Renumbering Files/main.py
+++ Organising Files/Renumbering Files/main.py
@@ -1,5 +1,5 @@
 """Program which renumber the files name."""
-import re
+import re, os
 from pathlib import Path
 
 p = Path("D:\PROGRAMMES\Code Arena\Automation\Organising Files\Renumbering Files")
@@ -7,8 +7,13 @@ p = Path("D:\PROGRAMMES\Code Arena\Automation\Organising Files\Renumbering Files
 #     if i not in (42, 86, 103):
 #         with open(f'spam{str(i).zfill(3)}.txt', 'w') as file:
 #             pass
-pattern = re.compile(r'\w+(\d{3})\.txt')
+pattern = re.compile(r'(\w+)(\d{3})\.txt')
 for idx, file_name in enumerate(p.glob("spam*.txt")):
     file_name = file_name.parts[-1]
+    f_name = file_name
     num = pattern.search(file_name)
-    num = num.group(1)
+    file_name = num.group(1)
+    num = num.group(2)
+    if int(num) != idx+1:
+        os.rename(p / f_name, f"{file_name}{idx+1}.txt")
+    print("Program runs successfully")
