4177b685c50023c89e3215e6177d394be5325d9c
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-26 08:12:08 +0530

    clear added

--- Designing And Deploying/snowstorm.py
+++ Designing And Deploying/snowstorm.py
@@ -5,3 +5,11 @@ BOTTOM = chr(9604)
 FULL = chr(9608)
 
 DENSITY = 4 # Default snow density set to 4
+if len(sys.argv) > 1:
+    DENSITY = int(sys.argv[1])
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+while True:
+    clear() # To clear the terminal window.
