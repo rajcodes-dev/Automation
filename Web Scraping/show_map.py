2e57ca93749ee79dd2001f528c5f9dbbc40cbe1e
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-27 09:03:22 +0530

    program added

--- Web Scraping/show_map.py
+++ Web Scraping/show_map.py
@@ -1,4 +1,8 @@
-import webbrowser, sys
+import webbrowser, sys, pyperclip
 
 if len(sys.argv) > 1:
     address = ' '.join(sys.argv[1:])
+else:
+    address = pyperclip.paste()
+
+webbrowser.open("https://www .openstreetmap .org /search ?query =" + address)
