34d0467c71863a9cf96e507a281a53e5327cb48f
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-28 11:02:22 +0530

    select tag

--- Web Scraping/searchpypi.py
+++ Web Scraping/searchpypi.py
@@ -5,3 +5,6 @@ import sys, webbrowser, requests, bs4
 print('Searching...')
 res = requests.get("https://pypi.org/search/?q=" + " ".join(sys.argv[1:]))
 res.raise_for_status()
+
+soup = bs4.BeautifulSoup('res.text', 'parser.html')
+link_elems = soup.select('.package-snippet')
