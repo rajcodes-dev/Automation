711dc857a9942af632f8c6a1f1474942ebbedf11
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-28 11:11:24 +0530

    open webbrowser

--- Web Scraping/searchpypi.py
+++ Web Scraping/searchpypi.py
@@ -8,3 +8,9 @@ res.raise_for_status()
 
 soup = bs4.BeautifulSoup('res.text', 'parser.html')
 link_elems = soup.select('.package-snippet')
+num_open = min(5, len(link_elems))
+
+for i in range(num_open):
+    url_to_open = 'https://pypi.org' + link_elems[i].get('href')
+    print("Opening", url_to_open)
+    webbrowser.open(url_to_open)
