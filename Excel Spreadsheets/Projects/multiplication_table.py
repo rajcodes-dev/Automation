c0229a4e67bcdc768d0dbedcf841e4d9f2888393
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-31 09:47:28 +0530

    boldfont added

--- Excel Spreadsheets/Projects/multiplication_table.py
+++ Excel Spreadsheets/Projects/multiplication_table.py
@@ -3,8 +3,10 @@ multiplication_table.py - program which display multiplication table
 on excel from command line arguments.
 """
 import sys, openpyxl
+from openpyxl.styles import Font
 
 TABLE = 0
+BOLD_FONT = Font(name='Times New Roman', bold=True)
 if int(sys.argv[1]) >= 1:
     TABLE = int(sys.argv[1])
 else:
@@ -17,8 +19,11 @@ sheet.title = 'Multiplication Table'
 
 for row_num in range(2, TABLE + 2):
     sheet.cell(row=row_num, column=1).value = row_num - 1
+    sheet.cell(row=row_num, column=1).font = BOLD_FONT
+
 for col_num in range(2, TABLE + 2):
     sheet.cell(row=1, column=col_num).value = col_num - 1
+    sheet.cell(row=1, column=col_num).font = BOLD_FONT
 
 for row_num in range(2, TABLE + 2):
     for col_num in range(2, TABLE + 2):
