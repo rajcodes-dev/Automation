e8a0d19fac898037aed2dbd72bb98b7010dd31ed
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-31 09:30:32 +0530

    outer loop completed

--- Excel Spreadsheets/Projects/multiplication_table.py
+++ Excel Spreadsheets/Projects/multiplication_table.py
@@ -7,5 +7,17 @@ import sys, openpyxl
 TABLE = 0
 if int(sys.argv[1]) >= 1:
     TABLE = int(sys.argv[1])
+else:
+    print("Argument must be integer")
+    sys.exit()
 
-print(TABLE)
+wb = openpyxl.Workbook()
+sheet = wb['Sheet']
+sheet.title = 'Multiplication Table'
+
+for row_num in range(2, TABLE + 2):
+    sheet.cell(row=row_num, column=1).value = row_num - 1
+for col_num in range(2, TABLE + 2):
+    sheet.cell(row=1, column=col_num).value = col_num - 1
+
+wb.save(F'Excel Spreadsheets/Projects/Table{TABLE}.xlsx')
