25bac0ff15c9eb5e293a0bf7b2c161be999d843b
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-02-01 09:54:24 +0530

    working prototype

--- Excel Spreadsheets/Projects/blankRowInserter.py
+++ Excel Spreadsheets/Projects/blankRowInserter.py
@@ -6,5 +6,8 @@ if len(sys.argv) != 4:
 N = int(sys.argv[1])
 M = int(sys.argv[2])
 file_name = sys.argv[3]
-
-
+print(file_name)
+wb = openpyxl.load_workbook(file_name)
+sheet = wb.active
+sheet.insert_rows(idx=N, amount=M)
+wb.save(f"ChangeTable.xlsx")
