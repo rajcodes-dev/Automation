9a6b851ad762407f7813d0513ebd433ef0e6f1db
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-02-02 09:29:56 +0530

    added auditboringcoins

---
+++ Google Sheets/Projects/auditBoringcoin.py
@@ -0,0 +1,17 @@
+import ezsheets
+ss = ezsheets.Spreadsheet('https://autbor.com/boringcoin')
+accounts = {}
+
+# Each row is a transactions, loop over each row.
+for row in ss.sheets[0].getRows():
+    sender, recipient, amount = row[0], row[1], row[2]
+    if sender == 'PRE-MINE':
+        accounts.setdefault(recipient, 0)
+        accounts['recipient'] += amount
+    else:
+        accounts.setdefault(sender, 0)
+        accounts.setdefault(recipient, 0)
+        accounts[sender] -= amount
+        accounts[recipient] += amount
+
+print(accounts)
