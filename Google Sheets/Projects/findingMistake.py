900300114fd6f1b1ab208bdaa5bbbb26c0aff615
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-02-03 09:49:20 +0530

    program complete

--- Google Sheets/Projects/findingMistake.py
+++ Google Sheets/Projects/findingMistake.py
@@ -1,9 +1,21 @@
 import ezsheets
 
 ss = ezsheets.Spreadsheet("1jDZEdvSIh4TmZxccyy0ZXrH-ELlrwq8_YYiZrEOB4jg")
-mistake_row = []
-for rowNum in range(2, ss.sheets[0].rowCount + 1):
-    if int(ss.sheets[0].getRow(2)[0]) * int(ss.sheets[0].getRow(2)[1]) != int(ss.sheets[0].getRow(2)[2]) :
-        mistake_row.append(rowNum)
+sheet = ss.sheets[0]
 
-print(mistake_row)
+print("Scanning rows for error....")
+
+for rowNum in range(2, sheet.rowCount + 1):
+    row_data = sheet.getRow(rowNum)
+
+    if row_data[0] == '':
+        break
+
+    beans_per_jar = int(row_data[0])
+    jars = int(row_data[1])
+    total_beans = int(row_data[2])
+
+    if beans_per_jar * jars != total_beans:
+        print(f"Mistake found at row {rowNum}!")
+        print(f"Calculation: {beans_per_jar} * {jars} should be {total_beans}")
+        break
