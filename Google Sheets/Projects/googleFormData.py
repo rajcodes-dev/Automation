a754228180c624b3b763c9d9dfca96eadad68567
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-02-03 08:18:15 +0530

    complete program

--- Google Sheets/Projects/googleFormData.py
+++ Google Sheets/Projects/googleFormData.py
@@ -3,5 +3,9 @@ import ezsheets
 ss = ezsheets.Spreadsheet("1Bi-5W9Q8NDZVsgoxRjSM8iyiHm_R3obyu4wp6NW1K3w")
 sheet = ss.sheets[0]
 
-sheet.columnCount = 6
-sheet.rowCount = 5
+email_lst = []
+for num in range(2, sheet.rowCount + 1):
+    email = sheet[f'C{num}']
+    if email not in email_lst:
+        email_lst.append(email)
+print("These are email who attempted quiz.\n", email_lst)
