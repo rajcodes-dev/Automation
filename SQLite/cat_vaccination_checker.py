73f21abd4fbcc0179c70d212987286738cd25f25
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-02-05 10:38:49 +0530

    database error solved

--- SQLite/cat_vaccination_checker.py
+++ SQLite/cat_vaccination_checker.py
@@ -15,3 +15,17 @@ WHERE rowid NOT IN (SELECT cat_id FROM vaccinations WHERE vaccine = 'rabies')
 
 for row in conn.execute(query_missing):
     print(f"- {row[0]}")
+
+print("\n--- DATABASE ERROR --- ")
+
+query_error = """
+SELECT cats.name, vaccinations.vaccine, vaccinations.date_administered, cats.birthdate
+FROM cats
+JOIN vaccinations ON cats.rowid = vaccinations.cat_id
+WHERE vaccinations.date_administered < cats.birthdate;
+"""
+
+for row in conn.execute(query_error):
+    print(f"ERROR: {row[0]} got {row[1]} on {row[2]} but was born {row[3]}")
+
+conn.close()
