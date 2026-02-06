d40decab401d1e97cea9e87fbf194db01ea2833f
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-02-06 07:17:10 +0530

    added try and except

--- PDF and Word Document/extractpdfimages.py
+++ PDF and Word Document/extractpdfimages.py
@@ -6,3 +6,12 @@ image_num = 0
 
 for i, page in enumerate(reader.pages):
     print(f"Reading page {i+1} - {len(page.images)} images found...")
+
+    try:
+        for image in page.images:
+            with open(f'{image_num}_image_{image.name}', 'wb') as file:
+                file.write(image.data)
+            print(f"Wrote {image_num}_image_{image.name}...")
+            image_num += 1
+    except Exception as exc:
+        print(f"Skipped page {i+1} due to error: {exc}")
