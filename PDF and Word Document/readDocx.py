28aaedc84d69a1a81417057b2dd6db93d42e69ec
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-02-08 08:44:31 +0530

    added readDocx

---
+++ PDF and Word Document/readDocx.py
@@ -0,0 +1,8 @@
+import docx
+
+def get_text(filename):
+    doc = docx.Document()
+    full_text = []
+    for para in doc.paragraphs:
+        full_text.append(para.text)
+    return '\n'.join(full_text)
