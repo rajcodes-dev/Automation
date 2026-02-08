2155fb5f65fc8196e11d77b38b5cdb32c38a560c
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-02-08 07:18:22 +0530

    copy all pages

--- PDF and Word Document/combine_pdfs.py
+++ PDF and Word Document/combine_pdfs.py
@@ -12,4 +12,6 @@ pdf_filenames.sort(key=str.lower)
 
 writer = pypdf.PdfWriter()
 
-
+for pdf_filename in pdf_filenames:
+    reader = pypdf.PdfReader(pdf_filename)
+    writer.append(pdf_filename, (1, len(reader.pages)))
