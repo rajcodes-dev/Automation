4ca82b9df4599454a96938aed810d39b0081844d
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-02-08 07:21:27 +0530

    combined pdf

--- PDF and Word Document/combine_pdfs.py
+++ PDF and Word Document/combine_pdfs.py
@@ -15,3 +15,6 @@ writer = pypdf.PdfWriter()
 for pdf_filename in pdf_filenames:
     reader = pypdf.PdfReader(pdf_filename)
     writer.append(pdf_filename, (1, len(reader.pages)))
+
+with open('combined.pdf', 'wb') as file:
+    writer.write(file)
